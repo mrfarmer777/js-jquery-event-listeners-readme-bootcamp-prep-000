5350ede4c6a3c1653cbed31504540491c99d5b13
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-18 02:50:19 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,36 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
+
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('input:first-of-type').on('keydown',function(e){
+    if(e.which===71){
+      alert('you done pressed a g');
+    }
+    
+  });
+  
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
