2477b1775e4cc611666278e32cc900633c2ca18f
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-18 02:38:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,21 @@
 $(document).ready(function(){
 
 // call functions here
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!');
+  });
+}
 
+function frameIt(){
+  $('img').on('load',function(){
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('input:first')
+  
+  
+}
 });
